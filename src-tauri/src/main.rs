85ded5f2ed47ba10a1eae5e4d054ad463c39a4eb
dayan <eam.dayan@gmail.com>
2023-02-07 22:56:59 +0700

    update: handle unknown command error

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use std::process::Command;
+use std::{env, path::Path, process::Command};
 
 // use std::process::Command;
 fn main() {
@@ -15,13 +15,37 @@ fn main() {
 
 #[tauri::command]
 fn take_command(name: &str) -> String {
-    let split: Vec<&str> = name.split_whitespace().collect();
-    let output = Command::new(split[0])
-        .args(&split[1..])
-        .output()
-        .expect("cannot run command");
+    let mut split = name.trim().split_whitespace();
 
-    let out = std::str::from_utf8(&output.stdout);
-    println!("{:?}", out.unwrap().to_string());
-    out.unwrap().to_string()
+    let command = split.next().unwrap();
+    let args = split;
+
+    let output = match command {
+        "cd" => {
+            // default to '/' as new directory if one was not provided
+            let new_dir = args.peekable().peek().map_or("/", |x| *x);
+            let root = Path::new(new_dir);
+            if let Err(e) = env::set_current_dir(&root) {
+                eprintln!("{}", e);
+            }
+            "".to_owned()
+        }
+        "exit" => {
+            std::process::exit(0x0100);
+        }
+        command => {
+            let output = Command::new(command).args(args).output();
+            println!("{:?}", &output);
+
+            match output {
+                Ok(shell_output) => {
+                    let out = std::str::from_utf8(&shell_output.stdout);
+                    out.expect("invalid command").to_string()
+                }
+                Err(err) => err.to_string(),
+            }
+        }
+    };
+
+    output
 }
